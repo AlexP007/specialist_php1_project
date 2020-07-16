190b2051a292f7e76f4f06789a85c77e260cec03
AlexP <alex.p.panteleev@gmail.com>
2020-07-16 20:00:25 +0300

    [i] implement style.css

--- index.php
+++ index.php
@@ -6,49 +6,45 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Резюме - {{имя}}</title>
+    <!-- CSS -->
+    <link href="assets/style.css" rel="stylesheet">
     <!-- Load Fonts -->
     <link href='https://fonts.googleapis.com/css?family=Roboto+Mono:400,100,300italic,300,100italic,400italic,500,500italic,700,700italic&amp;subset=latin,cyrillic' rel='stylesheet'>
 </head>
 <body>
 <!-- Container -->
-<div class="container">
-
-    <!-- Header -->
-    <header>
-        <div class="head">
-            <nav class="nav">
-                <ul>
-                    <li><a href="/?p=resume" class="lnk">Резюме</a></li>
-                    <li><a href="/?p=portfolio" class="lnk">Портфолио</a></li>
-                    <li><a href="/?p=blog" class="lnk">Блог</a></li>
-                    <li><a href="/?p=contacts" class="lnk">Контанты</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-
+<div class="container roboto bcg-orange">
     <!-- Wrapper -->
-    <div class="wrapper">
+    <div class="wrapper bcg-white">
 
-        <!-- Started -->
-        <section class="section started">
-            <div class="centrize full-width">
-                <div class="vertical-center">
-                    <div class="started-content">
-                        <h1>Привет! Меня зовут {{имя}}</h1>
-                        <p>Я профессиональный разработчик php</p>
-                    </div>
-                </div>
+        <header>
+            <div class="head">
+                <nav>
+                    <ul>
+                        <li><a href="/?p=resume" class="lnk">Резюме</a></li>
+                        <li><a href="/?p=portfolio" class="lnk">Портфолио</a></li>
+                        <li><a href="/?p=blog" class="lnk">Блог</a></li>
+                        <li><a href="/?p=contacts" class="lnk">Контанты</a></li>
+                    </ul>
+                </nav>
             </div>
-        </section>
+        </header>
 
+        <main>
+            <div class="main__center">
+               <div class="main__center-smallbox">
+                   <h1>Привет! Меня зовут Александр Пантелеев</h1>
+                   <p>Я профессиональный разработчик php</p>
+               </div>
+            </div>
+        </main>
+
+        <footer>
+            <small class="copy">© {{Дата}} {{имя}}</small>
+        </footer>
     </div>
-
-    <!-- Footer -->
-    <footer>
-        <small class="copy">© {{Дата}} {{имя}}</small>
-    </footer>
-
+    <!-- end Wrapper -->
 </div>
+<!-- end Container -->
 </body>
 </html>
