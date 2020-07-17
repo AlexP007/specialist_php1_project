fec857b23bd2d606cebc930b0083a68aa7237716
AlexP <alex.p.panteleev@gmail.com>
2020-07-17 20:36:53 +0300

    [i] resume page

--- pages/resume.php
+++ pages/resume.php
@@ -3,36 +3,30 @@
         <h1 class="align-center">Моё Резюме</h1>
     </div>
 </div>
-<a href="#section" class="mouse_btn font-green">
+<a href="#resume" class="mouse_btn font-green">
     <span class="ion ion-mouse"></span>
 </a>
-<div id="section" class="section about">
-    <div class="content">
-        <div class="title">
-            <div class="title_inner">About Me</div>
+<section id="resume" class="resume">
+    <h2 class="resume__title">Обо мне</h2>
+    <img class="resume__image" src="assets/images/man1.jpg" alt="" />
+    <div class="resume__about">
+        <p>
+            Lorem ipsum dolor sit amet, in quodsi vulputate pro. Ius illum vocent mediocritatem an, cule dicta iriure at. Ubique maluisset vel te,
+            his dico vituperata ut. Pro ei phaedrum maluisset. Ex audire suavitate has, ei quodsi tacimates sapientem sed, pri zril ubique ut. Te
+            cule tation munere noluisse. Enim torquatos ne pri, eum mollis salutandi corrumpit et, fugit apeirian duo ad.
+        </p>
+        <div class="resume__info">
+            <ul>
+                <li><strong class="font-green">Name:</strong> John Doe</li>
+                <li><strong class="font-green">Age:</strong> 22 Years</li>
+                <li><strong class="font-green">Job:</strong> Web Developer</li>
+                <li><strong class="font-green">Citizenship:</strong> Russian</li>
+                <li><strong class="font-green">Residence: </strong> Moscow</li>
+                <li><strong class="font-green">E-mail:</strong> johndoe@domain.com</li>
+            </ul>
         </div>
-        <div class="image">
-            <img src="images/man1.jpg" alt="" />
-        </div>
-        <div class="desc">
-            <p>
-                Lorem ipsum dolor sit amet, in quodsi vulputate pro. Ius illum vocent mediocritatem an, cule dicta iriure at. Ubique maluisset vel te,
-                his dico vituperata ut. Pro ei phaedrum maluisset. Ex audire suavitate has, ei quodsi tacimates sapientem sed, pri zril ubique ut. Te
-                cule tation munere noluisse. Enim torquatos ne pri, eum mollis salutandi corrumpit et, fugit apeirian duo ad.
-            </p>
-            <div class="info-list">
-                <ul>
-                    <li><strong>Name:</strong> John Doe</li>
-                    <li><strong>Age:</strong> 22 Years</li>
-                    <li><strong>Job:</strong> Web Developer</li>
-                    <li><strong>Citizenship:</strong> Ukraine</li>
-                    <li><strong>Residence: </strong> Kiev</li>
-                    <li><strong>E-mail:</strong> johndoe@domain.com</li>
-                </ul>
-            </div>
-            <div class="bts"><a href="#" class="btn fill" data-text="Download CV">Download CV</a></div>
-        </div>
-        <div class="clear"></div>
+        <a href="#" class="btn bcg-green font-white" data-text="Download CV">Download CV</a>
     </div>
-</div>
+    <div class="clear"></div>
+</section>
 
