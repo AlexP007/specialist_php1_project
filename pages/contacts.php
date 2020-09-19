a710029cd0aedd6fbb3939c0e40de3c0dcdff7c0
AlexP <alex.p.panteleev@gmail.com>
2020-09-19 20:56:32 +0300

    [i]

--- pages/contacts.php
+++ pages/contacts.php
@@ -2,4 +2,19 @@
     <div class="main__center-heading">
         <h1 class="align-center">Контакты</h1>
     </div>
+
+    <section>
+        <form method="post">
+            <div class="contacts__form-group">
+                <input type="text" name="form_text_1" placeholder="Имя*">
+            </div>
+            <div class="contacts__form-group">
+                <input type="email" name="form_email_2" placeholder="Email*">
+            </div>
+            <div class="contacts__form-group">
+                <textarea name="form_textarea_3" placeholder="Сообщение"></textarea>
+            </div>
+            <input class="btn bcg-green font-white roboto" type="submit" name="web_form_submit" value="Написать">
+        </form>
+    </section>
 </div>
--- pages/main.php
+++
@@ -1,6 +0,0 @@
-<div class="main__center">
-    <div class="main__center-heading">
-        <h1>Привет! Меня зовут <?= $fullName ?></h1>
-        <p>Я <?= $me['profession'] ?></p>
-    </div>
-</div>
